5a9af04ae8f252323a7cf538609aa3f1c2461cc2
Paolo <paolorodriguez1cano@gmail.com>
2026-05-09 15:25:20 -0500

    feat: numero aleatorio y primera comparacion if/else

--- script.js
+++ script.js
@@ -1,66 +1,28 @@
-let nombre = 'Paolo';
-let edad = 18;
-let estaAprendiendo = true;
+// === ADIVINA EL NÚMERO ===
+// El programa genera un número secreto entre 1 y 100
+// El jugador intenta adivinarlo
 
-console.log('mi nombre: ' + nombre);
-console.log('mi edad: ' + edad);
-console.log('estoy aprendiendo: ' + estaAprendiendo);
+console.log('- Adivina el Número -');
 
-console.log('Tipos de datos: ');
-console.log(typeof nombre); //tipo: string
-console.log(typeof edad); //tipo: number
-console.log(typeof estaAprendiendo); //tipo: boolean
+// Generar número secreto
+const numeroSecreto = Math.floor(Math.random() * 100) + 1;
 
-console.log('-----------------------------');
+//temporal
+console.log('(DEBUG) Número secreto:', numeroSecreto);
 
-const curso = 'Code 101';
-const maxIntentos = 10;
-//const maxIntentos = 20
+console.log('-----------------------');
 
-console.log('curso: ' + curso);
-console.log('máximo de intentos: ' + maxIntentos);
+let intento = prompt('Adivina el numero (1-100):');
+intento = Number(intento);
 
-console.log('-----------------------------');
+console.log('Intento del jugador:', intento);
+console.log('Tipo:', typeof intento);
 
-let nombreUsuario = prompt('¿Cómo te llamas?');
-let edadUsuario = prompt('¿Cuantos años tienes?');
+console.log('-----------------------');
 
-alert('Hola ' + nombreUsuario + ', tienes ' + edadUsuario + ' años');
-
-console.log('Nombre: ' + nombreUsuario);
-console.log('Edad: ' + edadUsuario);
-console.log(typeof edadUsuario); //prompt siempre devuelve string, incluso si el usuario ingresa un número.
-
-console.log('-----------------------------');
-
-let a = 20;
-let b = 7;
-
-console.log('Suma: ', a + b);
-console.log('Resta: ', a - b);
-console.log('Multiplicación: ', a * b);
-console.log('División: ', a / b);
-console.log('Módulo: ', a % b);
-
-console.log('-----------------------------');
-console.log('que resultados esperamos?');
-
-console.log('5' + 3);
-console.log(5 + 3);
-console.log('5' - 3);
-
-console.log('-----------------------------');
-//Forma 1
-let saludo1 = 'Hola ' + nombreUsuario + ', tienes ' + edadUsuario + ' años.';
-console.log(saludo1);
-//Forma 2
-let saludo2 = `Hola ${nombreUsuario}, tienes ${edadUsuario} años.`;
-console.log(saludo2);
-
-//Calculadora de edad
-let añoActual = 2026;
-let añoNacimiento = añoActual - Number(edadUsuario);
-
-
-// Completa el mensaje usando template literals:
-let mensaje = `Hola ${nombreUsuario}, naciste aproximadamente en ${añoNacimiento}.`;
+//primera comparacion
+if (intento === numeroSecreto) {
+    alert('🎉 ¡Correcto! El número era ' + numeroSecreto);
+}else{
+    alert('❌ Incorrecto. El número era ' + numeroSecreto);
+}
