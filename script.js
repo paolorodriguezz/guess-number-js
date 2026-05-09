afe3f1df4d0c7698084512d2af5e09da4fb02805
Paolo <paolorodriguez1cano@gmail.com>
2026-05-09 14:59:30 -0500

    feat: operadores aritmeticos, concatenacion y template literals

--- script.js
+++ script.js
@@ -25,8 +25,42 @@ console.log('-----------------------------');
 let nombreUsuario = prompt('¿Cómo te llamas?');
 let edadUsuario = prompt('¿Cuantos años tienes?');
 
-alert('Hola ' + [nombre] + ', tienes ' + [edad] + ' años');
+alert('Hola ' + nombreUsuario + ', tienes ' + edadUsuario + ' años');
 
 console.log('Nombre: ' + nombreUsuario);
 console.log('Edad: ' + edadUsuario);
 console.log(typeof edadUsuario); //prompt siempre devuelve string, incluso si el usuario ingresa un número.
+
+console.log('-----------------------------');
+
+let a = 20;
+let b = 7;
+
+console.log('Suma: ', a + b);
+console.log('Resta: ', a - b);
+console.log('Multiplicación: ', a * b);
+console.log('División: ', a / b);
+console.log('Módulo: ', a % b);
+
+console.log('-----------------------------');
+console.log('que resultados esperamos?');
+
+console.log('5' + 3);
+console.log(5 + 3);
+console.log('5' - 3);
+
+console.log('-----------------------------');
+//Forma 1
+let saludo1 = 'Hola ' + nombreUsuario + ', tienes ' + edadUsuario + ' años.';
+console.log(saludo1);
+//Forma 2
+let saludo2 = `Hola ${nombreUsuario}, tienes ${edadUsuario} años.`;
+console.log(saludo2);
+
+//Calculadora de edad
+let añoActual = 2026;
+let añoNacimiento = añoActual - Number(edadUsuario);
+
+
+// Completa el mensaje usando template literals:
+let mensaje = `Hola ${nombreUsuario}, naciste aproximadamente en ${añoNacimiento}.`;
